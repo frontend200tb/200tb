c2392aeef4f5364daed2cd9de5c3a16510fc7997
frontend200tb <frontend200tb@mail.ru>
2025-10-09 19:36:11 +0300

    feat:2025.10.09

--- src/components/alg/alg-conspect/ftl/js/data-pages.js
+++ src/components/alg/alg-conspect/ftl/js/data-pages.js
@@ -1,6 +1,28 @@
 // Создаем массив pages со страницами из aside меню
-import htmlFtl from './../pages/elem-ftl.html';
+import htmlF1   from './../pages/elem-f1.html';
+import htmlF2   from './../pages/elem-f2.html';
+import htmlF3   from './../pages/elem-f3.html';
+import htmlF4   from './../pages/elem-f4.html';
+import htmlF5   from './../pages/elem-f5.html';
+import htmlF6   from './../pages/elem-f6.html';
+import htmlF7   from './../pages/elem-f7.html';
+import htmlF8   from './../pages/elem-f8.html';
+import htmlF9   from './../pages/elem-f9.html';
+import htmlF10  from './../pages/elem-f10.html';
+import htmlF11  from './../pages/elem-f11.html';
+import htmlF12  from './../pages/elem-f12.html';
 
 export const pages = [
-  htmlFtl,
+  htmlF1,
+  htmlF2,
+  htmlF3,
+  htmlF4,
+  htmlF5,
+  htmlF6,
+  htmlF7,
+  htmlF8,
+  htmlF9,
+  htmlF10,
+  htmlF11,
+  htmlF12,
 ];
--- src/components/alg/alg-conspect/misis/js/data-pages.js
+++ src/components/alg/alg-conspect/misis/js/data-pages.js
@@ -1,6 +1,16 @@
 // Создаем массив pages со страницами из aside меню
-import htmlMisis from './../pages/elem-misis.html';
+import htmlM1 from './../pages/elem-m1.html';
+import htmlM2 from './../pages/elem-m2.html';
+import htmlM3 from './../pages/elem-m3.html';
+import htmlM4 from './../pages/elem-m4.html';
+import htmlM5 from './../pages/elem-m5.html';
+import htmlM6 from './../pages/elem-m6.html';
 
 export const pages = [
-  htmlMisis,
+  htmlM1,
+  htmlM2,
+  htmlM3,
+  htmlM4,
+  htmlM5,
+  htmlM6,
 ];
