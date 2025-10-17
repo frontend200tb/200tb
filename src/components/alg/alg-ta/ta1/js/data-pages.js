1469b496011db7ea73dab7a498e71d07c06eddc5
frontend200tb <frontend200tb@mail.ru>
2025-10-17 19:18:58 +0300

    feat:2025.10.17

--- src/components/alg/alg-ta/ta1/js/data-pages.js
+++ src/components/alg/alg-ta/ta1/js/data-pages.js
@@ -1,21 +1,27 @@
 // Создаем массив pages со страницами из aside меню
-import htmlL1   from './../pages/elem-l1.html';
-import htmlL2   from './../pages/elem-l2.html';
-import htmlL3   from './../pages/elem-l3.html';
-import htmlL4   from './../pages/elem-l4.html';
-import htmlR12  from './../pages/elem-r12.html';
-import htmlL5   from './../pages/elem-l5.html';
-import htmlL6   from './../pages/elem-l6.html';
-import htmlR34  from './../pages/elem-r34.html';
-import htmlL7   from './../pages/elem-l7.html';
-import htmlL8   from './../pages/elem-l8.html';
-import htmlR56  from './../pages/elem-r56.html';
-import htmlR78  from './../pages/elem-r78.html';
+import htmlL1     from './../pages/elem-l1.html';
+import htmlL1dz   from './../pages/elem-l1dz.html';
+import htmlL2     from './../pages/elem-l2.html';
+import htmlL2dz   from './../pages/elem-l2dz.html';
+import htmlL3     from './../pages/elem-l3.html';
+import htmlL3dz   from './../pages/elem-l3dz.html';
+import htmlL4     from './../pages/elem-l4.html';
+import htmlR12    from './../pages/elem-r12.html';
+import htmlL5     from './../pages/elem-l5.html';
+import htmlL6     from './../pages/elem-l6.html';
+import htmlR34    from './../pages/elem-r34.html';
+import htmlL7     from './../pages/elem-l7.html';
+import htmlL8     from './../pages/elem-l8.html';
+import htmlR56    from './../pages/elem-r56.html';
+import htmlR78    from './../pages/elem-r78.html';
 
 export const pages = [
   htmlL1,
+  htmlL1dz,
   htmlL2,
+  htmlL2dz,
   htmlL3,
+  htmlL3dz,
   htmlL4,
   htmlR12,
   htmlL5,
