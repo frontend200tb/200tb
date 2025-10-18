ce0bc4184cce849d9db2ffb5899e94f0d0afab24
frontend200tb <frontend200tb@mail.ru>
2025-10-18 21:02:24 +0300

    feat:2025:10.18

--- src/components/alg/alg-ta/ta1/js/data-pages.js
+++ src/components/alg/alg-ta/ta1/js/data-pages.js
@@ -6,12 +6,17 @@ import htmlL2dz   from './../pages/elem-l2dz.html';
 import htmlL3     from './../pages/elem-l3.html';
 import htmlL3dz   from './../pages/elem-l3dz.html';
 import htmlL4     from './../pages/elem-l4.html';
+import htmlL4dz   from './../pages/elem-l4dz.html';
 import htmlR12    from './../pages/elem-r12.html';
 import htmlL5     from './../pages/elem-l5.html';
+import htmlL5dz   from './../pages/elem-l5dz.html';
 import htmlL6     from './../pages/elem-l6.html';
+import htmlL6dz   from './../pages/elem-l6dz.html';
 import htmlR34    from './../pages/elem-r34.html';
 import htmlL7     from './../pages/elem-l7.html';
+import htmlL7dz   from './../pages/elem-l7dz.html';
 import htmlL8     from './../pages/elem-l8.html';
+import htmlL8dz   from './../pages/elem-l8dz.html';
 import htmlR56    from './../pages/elem-r56.html';
 import htmlR78    from './../pages/elem-r78.html';
 
@@ -23,12 +28,17 @@ export const pages = [
   htmlL3,
   htmlL3dz,
   htmlL4,
+  htmlL4dz,
   htmlR12,
   htmlL5,
+  htmlL5dz,
   htmlL6,
+  htmlL6dz,
   htmlR34,
   htmlL7,
+  htmlL7dz,
   htmlL8,
+  htmlL8dz,
   htmlR56,
   htmlR78,
 ];
--- src/components/alg/alg-ta/ta2/js/data-pages.js
+++ src/components/alg/alg-ta/ta2/js/data-pages.js
@@ -1,22 +1,54 @@
 // Создаем массив pages со страницами из aside меню
-import htmlL1     from './../pages/elem-l1.html';
-import htmlR12a   from './../pages/elem-r12a.html';
-import htmlR12b   from './../pages/elem-r12b.html';
-import htmlR34a   from './../pages/elem-r34a.html';
-import htmlR34b   from './../pages/elem-r34b.html';
-import htmlR56a   from './../pages/elem-r56a.html';
-import htmlR56b   from './../pages/elem-r56b.html';
-import htmlR78a   from './../pages/elem-r78a.html';
-import htmlR78b   from './../pages/elem-r78b.html';
+import htmlL1      from './../pages/elem-l1.html';
+import htmlL1adz   from './../pages/elem-l1adz.html';
+import htmlL2adz   from './../pages/elem-l2adz.html';
+import htmlR12a    from './../pages/elem-r12a.html';
+import htmlL1bdz   from './../pages/elem-l1bdz.html';
+import htmlL2bdz   from './../pages/elem-l2bdz.html';
+import htmlR12b    from './../pages/elem-r12b.html';
+import htmlL3adz   from './../pages/elem-l3adz.html';
+import htmlL4adz   from './../pages/elem-l4adz.html';
+import htmlR34a    from './../pages/elem-r34a.html';
+import htmlL3bdz   from './../pages/elem-l3bdz.html';
+import htmlL4bdz   from './../pages/elem-l4bdz.html';
+import htmlR34b    from './../pages/elem-r34b.html';
+import htmlL5adz   from './../pages/elem-l5adz.html';
+import htmlL6adz   from './../pages/elem-l6adz.html';
+import htmlR56a    from './../pages/elem-r56a.html';
+import htmlL5bdz   from './../pages/elem-l5bdz.html';
+import htmlL6bdz   from './../pages/elem-l6bdz.html';
+import htmlR56b    from './../pages/elem-r56b.html';
+import htmlL7adz   from './../pages/elem-l7adz.html';
+import htmlL8adz   from './../pages/elem-l8adz.html';
+import htmlR78a    from './../pages/elem-r78a.html';
+import htmlL7bdz   from './../pages/elem-l7bdz.html';
+import htmlL8bdz   from './../pages/elem-l8bdz.html';
+import htmlR78b    from './../pages/elem-r78b.html';
 
 export const pages = [
   htmlL1,
+  htmlL1adz,
+  htmlL2adz,
   htmlR12a,
+  htmlL1bdz,
+  htmlL2bdz,
   htmlR12b,
+  htmlL3adz,
+  htmlL4adz,
   htmlR34a,
+  htmlL3bdz,
+  htmlL4bdz,
   htmlR34b,
+  htmlL5adz,
+  htmlL6adz,
   htmlR56a,
+  htmlL5bdz,
+  htmlL6bdz,
   htmlR56b,
+  htmlL7adz,
+  htmlL8adz,
   htmlR78a,
+  htmlL7bdz,
+  htmlL8bdz,
   htmlR78b,
 ];
