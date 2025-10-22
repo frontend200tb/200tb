9bc8dad2344439e134f997f6350a032fecd4ab5a
frontend200tb <frontend200tb@mail.ru>
2025-10-22 18:51:12 +0300

    feat:2025.10.22

--- src/components/alg/alg-ta/ta4/js/data-pages.js
+++ src/components/alg/alg-ta/ta4/js/data-pages.js
@@ -3,20 +3,28 @@ import htmlL0dz   from './../pages/elem-l0dz.html';
 import htmlL1     from './../pages/elem-l1.html';
 import htmlL1dz   from './../pages/elem-l1dz.html';
 import htmlL2     from './../pages/elem-l2.html';
+import htmlL2dz   from './../pages/elem-l2dz.html';
 import htmlR0     from './../pages/elem-r0.html';
 import htmlL3     from './../pages/elem-l3.html';
+import htmlL3dz   from './../pages/elem-l3dz.html';
 import htmlL4     from './../pages/elem-l4.html';
+import htmlL4dz   from './../pages/elem-l4dz.html';
 import htmlR1     from './../pages/elem-r1.html';
 import htmlR2     from './../pages/elem-R2.html';
+import htmlL5dz   from './../pages/elem-l5dz.html';
 
 export const pages = [
   htmlL0dz,
   htmlL1,
   htmlL1dz,
   htmlL2,
+  htmlL2dz,
   htmlR0,
   htmlL3,
+  htmlL3dz,
   htmlL4,
+  htmlL4dz,
   htmlR1,
   htmlR2,
+  htmlL5dz,
 ];
--- src/components/alg/alg-ta/ta5/js/data-pages.js
+++ src/components/alg/alg-ta/ta5/js/data-pages.js
@@ -1,17 +1,19 @@
 // Создаем массив pages со страницами из aside меню
-import htmlL1   from './../pages/elem-l1.html';
-import htmlL2   from './../pages/elem-l2.html';
-import htmlR1   from './../pages/elem-r1.html';
-import htmlS1   from './../pages/elem-s1.html';
-import htmlL3   from './../pages/elem-l3.html';
-import htmlR2   from './../pages/elem-r2.html';
-import htmlL4   from './../pages/elem-l4.html';
-import htmlR3   from './../pages/elem-R3.html';
-import htmlS2   from './../pages/elem-s2.html';
-import htmlR4   from './../pages/elem-R4.html';
+import htmlL1    from './../pages/elem-l1.html';
+import htmlL1dz  from './../pages/elem-l1dz.html';
+import htmlL2    from './../pages/elem-l2.html';
+import htmlR1    from './../pages/elem-r1.html';
+import htmlS1    from './../pages/elem-s1.html';
+import htmlL3    from './../pages/elem-l3.html';
+import htmlR2    from './../pages/elem-r2.html';
+import htmlL4    from './../pages/elem-l4.html';
+import htmlR3    from './../pages/elem-R3.html';
+import htmlS2    from './../pages/elem-s2.html';
+import htmlR4    from './../pages/elem-R4.html';
 
 export const pages = [
   htmlL1,
+  htmlL1dz,
   htmlL2,
   htmlR1,
   htmlS1,
