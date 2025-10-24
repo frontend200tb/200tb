a837cdd2651d56d4778a0429f91a486a76888508
frontend200tb <frontend200tb@mail.ru>
2025-10-24 21:38:40 +0300

    feat:2025.10.24

--- src/components/alg/alg-ta/ta7/js/data-pages.js
+++ src/components/alg/alg-ta/ta7/js/data-pages.js
@@ -1,24 +1,32 @@
 // Создаем массив pages со страницами из aside меню
 import htmlL0dz   from './../pages/elem-l0dz.html';
 import htmlL1     from './../pages/elem-l1.html';
+import htmlL1dz   from './../pages/elem-l1dz.html';
 import htmlR1     from './../pages/elem-r1.html';
 import htmlL2     from './../pages/elem-l2.html';
+import htmlL2dz   from './../pages/elem-l2dz.html';
 import htmlR2     from './../pages/elem-r2.html';
 import htmlL3     from './../pages/elem-l3.html';
+import htmlL3dz   from './../pages/elem-l3dz.html';
 import htmlS1     from './../pages/elem-s1.html';
 import htmlR3     from './../pages/elem-R3.html';
 import htmlL4     from './../pages/elem-l4.html';
+import htmlL4dz   from './../pages/elem-l4dz.html';
 import htmlR4     from './../pages/elem-R4.html';
 
 export const pages = [
   htmlL0dz,
   htmlL1,
+  htmlL1dz,
   htmlR1,
   htmlL2,
+  htmlL2dz,
   htmlR2,
   htmlL3,
+  htmlL3dz,
   htmlS1,
   htmlR3,
   htmlL4,
+  htmlL4dz,
   htmlR4,
 ];
