19ed00b48ca27b62246d0e89b07c00fea952b7a7
frontend200tb <frontend200tb@mail.ru>
2025-11-10 21:14:00 +0300

    feat:2025.11.10

--- src/components/code/code-cpp/cpp-courses/js/data-aside.js
+++ src/components/code/code-cpp/cpp-courses/js/data-aside.js
@@ -5,4 +5,5 @@ export const asideThemes = [
   'metanit<br>Руководство по C++',
   'yandex<br>Основы C++',
   '2005 Майоров',
+  '2007 cplus-plus.ru',
 ];
--- src/components/code/code-cpp/cpp-courses/js/data-pages.js
+++ src/components/code/code-cpp/cpp-courses/js/data-pages.js
@@ -3,7 +3,8 @@ import htmlCourse1    from './../pages/elem-course-cpp-1.html';
 import htmlCourse2    from './../pages/elem-course-cpp-2.html';
 import htmlCourse3    from './../pages/elem-course-cpp-3.html';
 import htmlCourse4    from './../pages/elem-course-cpp-4.html';
-import html2005Major    from './../pages/elem-2005major.html';
+import html2005Major  from './../pages/elem-2005major.html';
+import html2007       from './../pages/elem-2007.html';
 
 export const pages = [
   htmlCourse1,
@@ -11,4 +12,5 @@ export const pages = [
   htmlCourse3,
   htmlCourse4,
   html2005Major,
+  html2007,
 ];
