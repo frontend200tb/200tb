752c7ee728616783bcc314cff87c7a70f88fdd7c
frontend200tb <frontend200tb@mail.ru>
2025-12-05 19:52:21 +0300

    feat:2025.12.05

--- src/components/alg/alg-conspect/mifi/js/data-pages.js
+++ src/components/alg/alg-conspect/mifi/js/data-pages.js
@@ -1,10 +1,12 @@
 // Создаем массив pages со страницами из aside меню
-import htmlM1tp from './../pages/elem-m1tp.html';
-import htmlM2dp from './../pages/elem-m2dp.html';
-import htmlM3zh from './../pages/elem-m3zh.html';
+import htmlM1tp    from './../pages/elem-m1tp.html';
+import htmlM2dp    from './../pages/elem-m2dp.html';
+import htmlM3zh    from './../pages/elem-m3zh.html';
+import htmlM4asd   from './../pages/elem-m4asd.html';
 
 export const pages = [
   htmlM1tp,
   htmlM2dp,
   htmlM3zh,
+  htmlM4asd,
 ];
