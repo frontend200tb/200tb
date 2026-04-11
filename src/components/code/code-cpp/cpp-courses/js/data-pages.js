4c8fb56a4c169c3937c914140b41b20ced48a8f7
frontend200tb <frontend200tb@mail.ru>
2026-04-11 21:13:30 +0300

    feat:2026.04.11

--- src/components/code/code-cpp/cpp-courses/js/data-pages.js
+++ src/components/code/code-cpp/cpp-courses/js/data-pages.js
@@ -3,20 +3,22 @@ import htmlCourse1    from './../pages/elem-course-cpp-1.html';
 import htmlCourse2    from './../pages/elem-course-cpp-2.html';
 import htmlCourse3    from './../pages/elem-course-cpp-3.html';
 import htmlCourse4    from './../pages/elem-course-cpp-4.html';
-import html2005Major  from './../pages/elem-2005major.html';
+import html2005M      from './../pages/elem-2005-majorov.html';
 import html2007       from './../pages/elem-2007.html';
 import html2008       from './../pages/elem-2008.html';
-import html2011_1     from './../pages/elem-2011-1.html';
-import html2011_2     from './../pages/elem-2011-2.html';
+import html2011A      from './../pages/elem-2011-alekseev.html';
+import html2011L      from './../pages/elem-2011-linski.html';
+import html2011S      from './../pages/elem-2011-soldatkin.html';
 
 export const pages = [
   htmlCourse1,
   htmlCourse2,
   htmlCourse3,
   htmlCourse4,
-  html2005Major,
+  html2005M,
   html2007,
   html2008,
-  html2011_1,
-  html2011_2,
+  html2011A,
+  html2011L,
+  html2011S,
 ];
