b6226ef979c9616eb7d4f08e3393442bbcc74fe5
frontend200tb <frontend200tb@mail.ru>
2026-05-18 19:17:22 +0300

    2026.05.18

--- src/components/code/code-cpp/cpp-javarush/js/data-pages.js
+++ src/components/code/code-cpp/cpp-javarush/js/data-pages.js
@@ -3,10 +3,12 @@ import htmlJavarush   from './../pages/elem-javarush.html';
 import htmlLevel1     from './../pages/elem-level1.html';
 import htmlLevel2     from './../pages/elem-level2.html';
 import htmlLevel3     from './../pages/elem-level3.html';
+import htmlLevel4     from './../pages/elem-level4.html';
 
 export const pages = [
   htmlJavarush,
   htmlLevel1,
   htmlLevel2,
   htmlLevel3,
+  htmlLevel4,
 ];
